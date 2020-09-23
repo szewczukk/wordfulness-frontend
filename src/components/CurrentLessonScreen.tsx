b47e83535a1ad9380b64fb5a1d0d78a045380a57
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2020-09-23 23:17:16 +0200

    CurrentLessonScreen splitted

--- src/components/CurrentLessonScreen.tsx
+++ src/components/CurrentLessonScreen.tsx
@@ -1,39 +1,9 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 
-import { fetchCurrentLesson } from '../store/currentLesson/actions';
-import { Store } from '../store';
+import FlashcardsList from './FlashcardsList';
 
-interface ParamTypes {
-	id: string;
-}
-
-export default () => {
-	const dispatch = useDispatch();
-	const currentLesson = useSelector((state: Store) => state.currentLesson);
-	const { id } = useParams<ParamTypes>();
-
-	useEffect(() => {
-		dispatch(fetchCurrentLesson(id));
-	}, [dispatch, id]);
-
-	const { name, created, flashcards } = currentLesson;
-
-	return (
-		<>
-			<h1>
-				{name} ({new Date(created).toLocaleString()})
-			</h1>
-			{flashcards ? (
-				flashcards.map((flashcard) => (
-					<p key={flashcard.id}>
-						{flashcard.back} - {flashcard.front}
-					</p>
-				))
-			) : (
-				<h1>No flashcards!</h1>
-			)}
-		</>
-	);
-};
+export default () => (
+	<>
+		<FlashcardsList />
+	</>
+);
